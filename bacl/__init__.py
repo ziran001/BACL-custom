1cf43c736b6a24ad88b2cbed3ebe6131a158681b
ziran001 <zebra00229@gmail.com>
2026-09-09 09:04:20 +0800

    Add bacl/__init__.py

---
+++ bacl/__init__.py
@@ -0,0 +1,12 @@
+"""Modern BACL components for TorchVision Faster R-CNN."""
+
+from .data import YoloDetectionDataset, collate_fn, resolve_dataset_config
+from .model import build_bacl_fasterrcnn, freeze_for_classifier_stage
+
+__all__ = [
+    "YoloDetectionDataset",
+    "build_bacl_fasterrcnn",
+    "collate_fn",
+    "freeze_for_classifier_stage",
+    "resolve_dataset_config",
+]
